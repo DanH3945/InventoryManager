7e1a71662f9ff391e4f2a995ab01fc5275f641df
Heretic <hereticpurge@gmail.com>
2018-08-30 02:53:13 -0600

    Added Functionality to the Edit Fragment and continued building the Edit layout.

--- app/src/main/java/com/hereticpurge/inventorymanager/fragments/EditFragment.java
+++ app/src/main/java/com/hereticpurge/inventorymanager/fragments/EditFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.hereticpurge.inventorymanager.R;
 import com.hereticpurge.inventorymanager.model.ProductItem;
@@ -19,13 +21,21 @@ import com.hereticpurge.inventorymanager.model.ProductViewModel;
 
 public class EditFragment extends Fragment {
 
-    ProductViewModel mViewModel;
+    private ProductViewModel mViewModel;
 
-    ProductItem mProductItem;
+    private ProductItem mProductItem;
 
-    Button mSaveButton;
+    private EditText mName;
+    private EditText mBarcode;
+    private EditText mCustomId;
+    private EditText mCost;
+    private EditText mRetail;
+    private EditText mCurrentStock;
+    private EditText mTargetStock;
 
-    EditText mProductName;
+    private Switch mTrackSwitch;
+
+    private Button mSaveButton;
 
     public static EditFragment createInstance(@Nullable ProductItem productItem){
         EditFragment editFragment = new EditFragment();
@@ -41,10 +51,25 @@ public class EditFragment extends Fragment {
         mSaveButton = view.findViewById(R.id.edit_save_button);
         mSaveButton.setOnClickListener(v -> doSave());
 
-        mProductName = view.findViewById(R.id.edit_product_name_et);
+        mName = view.findViewById(R.id.edit_product_name_et);
+        mBarcode = view.findViewById(R.id.edit_product_barcode_et);
+        mCustomId = view.findViewById(R.id.edit_product_customid_et);
+        mCost = view.findViewById(R.id.edit_fragment_cost_et);
+        mRetail = view.findViewById(R.id.edit_fragment_retail_et);
+        mCurrentStock = view.findViewById(R.id.edit_fragment_stock_current_et);
+        mTargetStock = view.findViewById(R.id.edit_fragment_stock_target_et);
+
+        mTrackSwitch = view.findViewById(R.id.edit_fragment_track_switch_button);
 
         if (mProductItem != null) {
-            mProductName.setText(mProductItem.getName());
+            mName.setText(mProductItem.getName());
+            mBarcode.setText(mProductItem.getBarcode());
+            mCustomId.setText(mProductItem.getCustomId());
+            mCost.setText(mProductItem.getCost());
+            mRetail.setText(mProductItem.getRetail());
+            mCurrentStock.setText(String.valueOf(mProductItem.getCurrentStock()));
+            mTargetStock.setText(String.valueOf(mProductItem.getTargetStock()));
+            mTrackSwitch.setChecked(mProductItem.isTracked());
         }
 
         return view;
@@ -63,18 +88,30 @@ public class EditFragment extends Fragment {
         if (mProductItem == null){
             mProductItem = new ProductItem();
         }
-        mProductItem.setName(mProductName.getText().toString());
-
-        mViewModel.addProduct(mProductItem);
 
         try {
-            InputMethodManager inputMethodManager =
-                    (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(getView().getWindowToken(), 0);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+            mProductItem.setName(mName.getText().toString());
+            mProductItem.setBarcode(mBarcode.getText().toString());
+            mProductItem.setCustomId(mCustomId.getText().toString());
+            mProductItem.setCost(mCost.getText().toString());
+            mProductItem.setRetail(mRetail.getText().toString());
+            mProductItem.setCurrentStock(Integer.parseInt(mCurrentStock.getText().toString()));
+            mProductItem.setTargetStock(Integer.parseInt(mTargetStock.getText().toString()));
+            mProductItem.setTracked(mTrackSwitch.isChecked());
 
-        getActivity().onBackPressed();
+            mViewModel.addProduct(mProductItem);
+
+            try {
+                InputMethodManager inputMethodManager =
+                        (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+
+            getActivity().onBackPressed();
+        } catch (NumberFormatException nfe){
+            Toast.makeText(getContext(), R.string.number_error, Toast.LENGTH_LONG).show();
+        }
     }
 }
